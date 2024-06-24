2601d0b055175d44c516a5ee3a85d16ae8f4784e
FrancoisGgg <goensfrancois@gmail.com>
2024-06-24 14:55:20 +0200

    speed setpoint

--- src/controller.erl
+++ src/controller.erl
@@ -2,17 +2,14 @@
 
 % -export([pid/5]).
 -export([init/1]).
--export([controller_PID/1,controller_kalman/1,controller_complem/1]).
+-export([controller_kalman/1,controller_complem/1]).
 
--export([modif_coef/1,modif_coef2/1]).
--export([print_coef/0, print_coef2/0, pause_ctrl/1]).
+-export([modif_coef/1]).
+-export([pause_ctrl/1]).
 
 -define(Speed_Limit, 50.0).
 -define(Dist_Coef, 0.0).
-% -define(Speed_Coef, 2.0).
-% -define(Angle_Coef_P, 0.7).
-% -define(Angle_Coef_I, 0.7).
-% -define(Angle_Coef_D, 4.9).
+
 -define(Coef_Filter, 0.667).
 % -define(K, 0.97).
 
@@ -21,24 +18,13 @@
 
 
 init({Cal}) ->
-    % I2Cbus = grisp_i2c:open(i2c1),
-
     ets:new(variables, [set, public, named_table]),
-    % ets:insert(variables, {"Bus", I2Cbus}),
 
-    % ets:insert(variables, {"SpeedMes", 0.0}),
     ets:insert(variables, {"Angle_Rate", 0.0}),
     ets:insert(variables, {"Angle", 0.0}),
     ets:insert(variables, {"DC_Bias", Cal}),
-    ets:insert(variables, {"Offset", 0.0}),
     ets:insert(variables, {"AngleInt", 0.0}),
     ets:insert(variables, {"Angle_kalman", 0.0}),
-
-    ets:insert(variables, {"Angle_Coef_P", 0.0}),
-    ets:insert(variables, {"Angle_Coef_I", 0.0}),
-    ets:insert(variables, {"Angle_Coef_D", 0.0}),
-    ets:insert(variables, {"Offset_Coef", 0.99}),
-    ets:insert(variables, {"Speed_Coef", 0.0}),
     
 
     ets:insert(variables, {"Kp1", 0.0}),
@@ -49,24 +35,11 @@ init({Cal}) ->
     
     ets:insert(variables, {"PID_error_int", 0.0}),
     ets:insert(variables, {"Angle_Offset", 2.7}),
-    
+    ets:insert(variables, {"Input_speed", 0.0}),
+    ets:insert(variables, {"Speed_time", 0.0}),
 
     ets:insert(variables, {"Reset", 1.0}),
     ok.
-
-
-controller_PID(Measures) ->
-
-    {Ax,Az,Gy,Speed,Dt,Th} = Measures,
-
-    compute_angle({Ax,Az,Gy,Speed,Dt}),
-    compute_angle_offset(),
-
-    [{_,Reset}] = ets:lookup(variables, "Reset"),
-    
-    Acc = balance_controller_PID(Dt,Speed),
-    % io:format("Acc command: ~p~n",[Acc]),
-    {Acc, Reset}.
 
 controller_kalman(Measures) ->
 
@@ -75,8 +48,6 @@ controller_kalman(Measures) ->
 
     compute_angle({Ax,Az,Gy,Speed,Dt}),             %For graphs
     [{_,Angle}] = ets:lookup(variables, "Angle"),   %For graphs
-
-    compute_angle_offset(),
 
     [{_,Reset}] = ets:lookup(variables, "Reset"),
     Acc = balance_controller_kalman(Dt,Speed),
@@ -88,7 +59,6 @@ controller_complem(Measures) ->
     {Ax,Az,Gy,Speed,Dt} = Measures,
 
     compute_angle({Ax,Az,Gy,Speed,Dt}),
-    compute_angle_offset(),
 
     [{_,Angle}] = ets:lookup(variables, "Angle"),   %For graphs
 
@@ -101,75 +71,23 @@ controller_complem(Measures) ->
 pause_ctrl(R) ->
     [{_,Angle}] = ets:lookup(variables, "Angle"),
     ets:insert(variables, {"Reset", R}),
-    ets:insert(variables, {"Offset", Angle}),
     ets:insert(variables, {"PID_error_int", 0.0}),
     ok.
 
-
-modif_coef({P,I,D,F,S}) ->
-    ets:insert(variables, {"Angle_Coef_P", P}),
-    ets:insert(variables, {"Angle_Coef_I", I}),
-    ets:insert(variables, {"Angle_Coef_D", D}),
-    ets:insert(variables, {"Offset_Coef", F}),
-    ets:insert(variables, {"Speed_Coef", S}),
+set_speed(Speed, Time) ->
+    ets:insert(variables, {"Input_speed", Speed}),
+    ets:insert(variables, {"Speed_time", erlang:system_time()/1.0e6+Time}),
     ok.
 
-print_coef() ->
-    [{_,P}] = ets:lookup(variables, "Angle_Coef_P"),
-    [{_,I}] = ets:lookup(variables, "Angle_Coef_I"),
-    [{_,D}] = ets:lookup(variables, "Angle_Coef_D"),
-    [{_,F}] = ets:lookup(variables, "Offset_Coef"),
-    [{_,S}] = ets:lookup(variables, "Speed_Coef"),
-    io:format("Coefs: ~p, ~p, ~p, ~p, ~p~n",[P,I,D,F,S]),
-    ok.
-
-modif_coef2({Kp1,Ki1,Kp2,Kd2,K}) ->
+modif_coef({Kp1,Ki1,Kp2,Kd2,K}) ->
     ets:insert(variables, {"Kp1", Kp1}),
     ets:insert(variables, {"Ki1", Ki1}),
     ets:insert(variables, {"Kp2", Kp2}),
     ets:insert(variables, {"Kd2", Kd2}),
     ets:insert(variables, {"K", K}),
+
+    ets:insert(variables, {"PID_error_int", 0.0}),
     ok.
-
-print_coef2() ->
-    [{_,Kp1}] = ets:lookup(variables, "Kp1"),
-    [{_,Ki1}] = ets:lookup(variables, "Ki1"),
-    [{_,Kp2}] = ets:lookup(variables, "Kp2"),
-    [{_,Kd2}] = ets:lookup(variables, "Kd2"),
-    [{_,K}] = ets:lookup(variables, "K"),
-    io:format("Coefs: ~p, ~p, ~p, ~p, ~p~n",[Kp1,Ki1,Kp2,Kd2,K]),
-    ok.
-
-balance_controller_PID(Dt,Speed) ->
-
-    [{_,Offset}] = ets:lookup(variables, "Offset"),
-    [{_,Angle_Rate}] = ets:lookup(variables, "Angle_Rate"),
-    [{_,Angle}] = ets:lookup(variables, "Angle"),
-    [{_,Speed_Coef}] = ets:lookup(variables, "Speed_Coef"),
-    [{_,AngleInt}] = ets:lookup(variables, "AngleInt"),
-
-    Distance_saturated = 0.0, %saturation((distances[0] + distances[2]) / 2, 20),
-    % AngleRateError = (Angle + Offset) * ?Angle_Coef_P + (AngleInt) * ?Angle_Coef_I + (Angle_Rate) * ?Angle_Coef_D,
-
-    [{_,P}] = ets:lookup(variables, "Angle_Coef_P"),
-    [{_,I}] = ets:lookup(variables, "Angle_Coef_I"),
-    [{_,D}] = ets:lookup(variables, "Angle_Coef_D"),
-    % AngleRateError = (Angle + Offset) * P + (AngleInt) * I + (Angle_Rate) * D,
-    ErrorP = (Offset-Angle),
-    ErrorI = AngleInt + ErrorP * Dt,
-    ErrorD = -Angle_Rate,
-
-
-    PID_output = P*ErrorP + I*ErrorI + D*ErrorD,
-
-    io:format("~.3f, ~.3f~n",[Speed,Angle]),
-    % PID_output = P*ErrorP + I*ErrorI + D*ErrorD,
-    ets:insert(variables, {"AngleInt", ErrorI}),
-
-    Acc_comm = ( PID_output
-                         + ?Dist_Coef * Distance_saturated
-                         + Speed_Coef * Speed),
-    Acc_comm.
 
 balance_controller_kalman(Dt,Speed) ->
 
@@ -193,8 +111,19 @@ balance_controller_complem(Dt,Speed) ->
     [{_,Ki1}] = ets:lookup(variables, "Ki1"),
     [{_,Kp2}] = ets:lookup(variables, "Kp2"),
     [{_,Kd2}] = ets:lookup(variables, "Kd2"),
+    [{_,Input_speed}] = ets:lookup(variables, "Input_speed"),
+    [{_,Speed_time}] = ets:lookup(variables, "Speed_time"),
 
-    Target_angle = speed_PI(Dt,Speed,0,Kp1,Ki1),
+    T = erlang:system_time()/1.0e6,
+    if   
+        T < Speed_time ->
+            Speed_setpoint = Input_speed;
+        true ->
+            Speed_setpoint = 0
+    end,
+
+    Target_angle = speed_PI(Dt,Speed,Speed_setpoint,Kp1,Ki1),
+
     Acc = stability_PD(Dt,Angle_complem,Target_angle,Kp2,Kd2),
 
     io:format("~.3f, ~.3f, ~.3f~n",[Speed,Target_angle,Angle_complem]),
@@ -206,7 +135,7 @@ speed_PI(Dt,Speed,SetPoint,Kp,Ki) ->
     [{_,PID_error_int}] = ets:lookup(variables, "PID_error_int"),
 
     Error = SetPoint - Speed,
-    PID_error_int_new = PID_error_int + Error * Dt,
+    PID_error_int_new = saturation(PID_error_int + Error * Dt, 30),
     ets:insert(variables, {"PID_error_int", PID_error_int_new}),
     PI_output = Kp * Error + Ki * PID_error_int_new,
     PI_output.
@@ -254,34 +183,4 @@ compute_angle({Ax,Az,Gy,Speed,Dt}) ->
 
 
     ok.
-
-
-compute_angle_offset() ->
-
-    %Retreive values from table
-    [{_,Angle}] = ets:lookup(variables, "Angle"),
-    [{_,Offset}] = ets:lookup(variables, "Offset"),
-    [{_,Offset_Coef}] = ets:lookup(variables, "Offset_Coef"),
-    % New_Offset = ( Offset -Angle) * Offset_Coef + Angle
-    % New_Offset = (( Angle - Offset) * Offset_Coef) - Angle,
-    %New_Offset = (Offset_Coef - 1) * Angle - Offset_Coef * Offset,
-    New_Offset = ( Offset -Angle) * Offset_Coef + Angle,
-    ets:insert(variables, {"Offset", New_Offset}),
-    ok.
-
-
-
-
-
-
-% pid({Kp, Ki, Kd}, {Ierr0, Derr0}, Set_Point, Measure, Dt) ->
-
-%     Error = Set_Point - Measure,
-%     Ierr1 = Ierr0 + Error * Dt,
-%     Derr1 = (Error-Derr0)/Dt,
-
-%     Command = Kp * Error + Ki * Ierr1 + Kd * Derr1,
-%     {Command, {Ierr1, Error}}.
-
-
 
